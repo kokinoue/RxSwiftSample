afae98035ad18fd67425f2915221e8c8197ba719
koki inoue <ko.18.5in@gmail.com>
2020-02-13 23:04:50 +0900

    Initialize repository

--- RxSwiftSample/ViewController.swift
+++ RxSwiftSample/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.brown
+
+        let textField = UITextField()
+        textField.frame = CGRect(x:((self.view.bounds.width-320)/2),y:300,width:320,height:50)
+        textField.backgroundColor = UIColor.white
+        
+        let label = UILabel()
+        label.frame = CGRect(x:((self.view.bounds.width-320)/2),y:400,width:320,height:50)
+        label.backgroundColor = UIColor.green
+        label.textColor = UIColor.white
+        
+        view.addSubview(textField)
+        view.addSubview(label)
+        
+        textField.rx.text.orEmpty
+        .map {$0.description}
+        .bind(to: label.rx.text)
+        .disposed(by: disposeBag)
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
